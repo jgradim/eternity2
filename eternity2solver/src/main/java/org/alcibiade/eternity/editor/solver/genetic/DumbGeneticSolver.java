db096bd0df1d7da193aa7177fc6881c8b157b69f
Mário Carneiro <mario@magi.(none)>
2010-06-27 17:31:56 +0100

    Continued work on genetic algorit skeletons

--- eternity2solver/src/main/java/org/alcibiade/eternity/editor/solver/genetic/DumbGeneticSolver.java
+++ eternity2solver/src/main/java/org/alcibiade/eternity/editor/solver/genetic/DumbGeneticSolver.java
@@ -12,10 +12,31 @@ import org.alcibiade.eternity.editor.solver.genetic.GeneticSolver;
 public class DumbGeneticSolver extends GeneticSolver {
 
 	
-	public DumbGeneticSolver(GridModel grid, GridModel solutionGrid, ClusterManager clusterManager, int populationSize) {
+	public DumbGeneticSolver(GridModel grid, GridModel solutionGrid,
+		ClusterManager clusterManager, int populationSize) {
+		
 		super(grid, solutionGrid, clusterManager, populationSize);
 	}
 	
+	public void run() {
+		notifyStart();
+		clusterManager.showStartMessage();
+		
+		GridModel individualA = getMostFitIndividual();
+		GridModel individualB = getMostFitIndividual();
+		
+		Set<GridModel> children = crossing(individualA, individualB, 2);
+		
+		
+		notifyEnd(true);
+	}
+	
+
+	private void crossing(GridModel parentA, gridModel parentB, int children) {
+		
+	
+	
+	}
 
 	public String getSolverName() {
 		return "Dumb Genetic Solver";
--- eternity2solver/src/main/java/org/alcibiade/eternity/editor/solver/genetic/GeneticSolver.java
+++ eternity2solver/src/main/java/org/alcibiade/eternity/editor/solver/genetic/GeneticSolver.java
@@ -7,6 +7,7 @@ package org.alcibiade.eternity.editor.solver.genetic;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Iterator;
 import org.alcibiade.eternity.editor.model.GridModel;
 import org.alcibiade.eternity.editor.solver.ClusterManager;
 import org.alcibiade.eternity.editor.solver.EternitySolver;
@@ -19,25 +20,79 @@ public abstract class GeneticSolver extends EternitySolver {
 	protected long iterations;
 	
 	public GeneticSolver(GridModel grid, GridModel solutionGrid, ClusterManager clusterManager, int populationSize) {
-		super(clusterManager);
+		super(clusterManager);	
 		
 		this.problemGrid = grid;
 		this.solutionGrid = solutionGrid;
+		///System.out.println(grid.toQuadString());
 		
 		//hashset ok but GridModel does not implement comparabel or equals.
 		this.population = new HashSet<GridModel>();
 		
-		populate(populationSize);
+		populate(populationSize, grid);
 	}
 	
-	private void populate(int elements) {
-		int width = problemGrid.getSize();
-		
-		
-		while (population.size() < elements) {
-			GridModel somegrid = new GridModel()
+	private void populate(int populationSize, GridModel theGrid) {
+	
+		//fitness(theGrid);
+
+
+	
+		while (population.size() < populationSize) {
+			GridModel somegrid = theGrid.clone();
+			somegrid.shuffle();
 			population.add(somegrid);
+			
+			System.out.println("---");
+			System.out.println(somegrid.toQuadString());
+			System.out.println("---");			
+			
 		}
+	
+	}
+	
+	/**
+	 * returns the fitness of a grid (0..1)
+	 */
+	public float fitness(GridModel grid) {
+		
+//		System.out.println("--------- Fitness for --------------");
+//		
+//		System.out.println(grid.toQuadString());
+		
+		float connections = grid.countConnections();
+		float pairs = grid.countPairs();
+		float fitness = pairs / connections;
+//		System.out.printf("Conn: %.0f\tPairs: %.0f\tFitness: %f\n", connections, pairs, fitness);
+//		System.out.println("------------------------------------");
+
+		return fitness;
+	}
+	
+	/**
+	 * removes from the population and returns the most fit individual. 
+	 */
+	public GridModel getMostFitIndividual() {
+		
+		assert this.population.size() > 0;
+		
+		Iterator it = this.population.iterator();
+		GridModel bestIndividual = null;
+		float bestFitness = 0;
+		
+		do {
+			GridModel thisIndividual = (GridModel)it.next();
+			float thisFitness = fitness(thisIndividual);
+			
+			if (thisFitness >= bestFitness) {
+				bestIndividual = thisIndividual;
+				bestFitness = thisFitness;
+			}
+		} while (it.hasNext());
+		
+		/* this should never return null anyway. */
+		this.population.remove(bestIndividual);
+		return bestIndividual;
 	}
 	
 	public long getIterations() {
